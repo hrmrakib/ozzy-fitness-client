2006b128729793c7f89f2ed6a636c60cb9e64c79
hrmrakib <hrmrakibs@gmail.com>
2025-02-25 21:08:16 +0600

    rm: vercel.json

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,28 +1,3 @@
-// import { dirname } from "path";
-// import { fileURLToPath } from "url";
-// import { FlatCompat } from "@eslint/eslintrc";
-
-
-
-// const __filename = fileURLToPath(import.meta.url);
-// const __dirname = dirname(__filename);
-
-// const compat = new FlatCompat({
-//   baseDirectory: __dirname,
-// });
-
-// const eslintConfig = [
-//   ...compat.extends("next/core-web-vitals", "next/typescript"),
-//   {
-//     rules: {
-//       "@typescript-eslint/no-explicit-any": "off"
-//     }
-//   }
-// ];
-
-// export default eslintConfig;
-
-
 
 import globals from "globals";
 import pluginJs from "@eslint/js";
